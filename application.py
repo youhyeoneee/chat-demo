8f51038f97691bbf2f451994dd66bd2d7ec1570e
youhyeoneee <khukyh2018@khu.ac.kr>
2024-01-10 15:54:48 +0900

    feat: chatgpt api 연결

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import json
 from broadcaster import Broadcast
 from starlette.applications import Starlette
 from starlette.concurrency import run_until_first_complete
@@ -7,6 +8,9 @@ from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
+import requests
+import aiohttp
+import asyncio
 
 
 load_dotenv()  # Load .env variables
@@ -45,6 +49,18 @@ async def chatroom_ws_sender(websocket, channel_name):
         async for event in subscriber:
             await websocket.send_text(
                 event.message
+            )
+            await websocket.send_text(
+                '{"action":"message","user":"안내사항","message":"봇이 입력 중입니다"}'
+            )
+            params = {"m": json.loads(event.message)['message']}
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://3iebegq3s7.execute-api.ap-northeast-2.amazonaws.com/default/lambda-test", params=params) as resp:
+                    r = await resp.json()
+            bot_message = r['choices'][0]['message']['content']
+            bot_message = json.dumps(bot_message, ensure_ascii=False)
+            await websocket.send_text(
+                f'{{"action":"message","user":"신한투자증권 프로봇","message":{bot_message}}}'
             )
 
 
